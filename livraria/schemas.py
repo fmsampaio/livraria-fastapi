b60be8916474be5bcccfbee4072ac7202251c791
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2022-08-26 09:12:41 -0300

    Insert id info at show models

--- livraria/schemas.py
+++ livraria/schemas.py
@@ -5,16 +5,30 @@ class Editora(BaseModel):
     nome: str
     site: str
 
+class Autor(BaseModel):
+    nome: str
+
+class Categoria(BaseModel):
+    descricao: str
+
+class EditoraShow(BaseModel):
+    id: int
+    nome: str
+    site: str
+
     class Config():
         orm_mode = True
 
-class Autor(BaseModel):
+class AutorShow(BaseModel):
+    id: int
     nome: str
 
     class Config():
         orm_mode = True
 
-class Categoria(BaseModel):
+
+class CategoriaShow(BaseModel):
+    id: int
     descricao: str
 
     class Config():
@@ -35,9 +49,9 @@ class LivroShow(BaseModel):
     ISBN: str
     quantidade: int
     preco: float
-    editora: Editora
-    categoria: Categoria
-    autores: List[Autor] = []
+    editora: EditoraShow
+    categoria: CategoriaShow
+    autores: List[AutorShow] = []
 
     class Config():
         orm_mode = True
